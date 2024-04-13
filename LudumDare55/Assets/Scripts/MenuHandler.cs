9e9f835d3fdc09e39fedb3e97dabf3bc232f4548
Jude Holt <jude@holt-info.com>
2024-04-13 12:20:03 -0400

    Added Options scroll
    The options menu can now use a coroutine to scroll up from the bottom of the screen. Has not been calibrated whatsoever.

--- LudumDare55/Assets/Scripts/MenuHandler.cs
+++ LudumDare55/Assets/Scripts/MenuHandler.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuHandler : MonoBehaviour
 {
 
+    // How fast the Options menu scrolls up and down when opened and closed, in percent
+    [SerializeField] private float optionsSpeed = 1f;
+
+    // Various UI elements or groups of UI elements for activating and deactivating
     [SerializeField] private Canvas mainCanvas;
+    [SerializeField] private Image background;
     [SerializeField] private Canvas mainMenuSet;
     [SerializeField] private Canvas optionsMenuSet;
 
@@ -21,13 +27,31 @@ public class MenuHandler : MonoBehaviour
         // Trigger transition animation here, then once complete...
 
         // Load main play scene.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
 
     public void OpenOptions()
     {
         optionsMenuSet.enabled = true;
+
+        StartCoroutine(OptionsArrive());
+
         mainMenuSet.enabled = false;
+    }
+
+    // Coroutine for sliding the options up from the bottom of the screen
+    IEnumerator OptionsArrive()
+    {
+        float progress = 0f;
+
+        while (progress < 1f)
+        {
+            print(progress);
+            progress += 0.01f * optionsSpeed;
+            optionsMenuSet.transform.Translate(Vector3.up * progress);
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 
     public void CloseOptions()
