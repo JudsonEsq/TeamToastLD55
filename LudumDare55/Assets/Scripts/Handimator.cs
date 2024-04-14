05f3a31748356ba78a8feb3bc76608920909873b
Enigma <49351666+AwokeinanEnigma@users.noreply.github.com>
2024-04-14 00:53:48 -0400

    add hands add hands material & psd make viewmodel folder for the hands & its animations add handimator script for animating hands easily from other scripts add layer for hands botched attempts at trying to get hands to render above everything with the RenderAboveEverything script & camera stacking technique

---
+++ LudumDare55/Assets/Scripts/Handimator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+public class Handimator : MonoBehaviour{
+	public Animator Animator;
+	public void PlayDropAnimation(){
+		Animator.Play("Armature_Drop");
+	}
+	
+	public void PlayHoldAnimation(){
+		Animator.Play("Armature_Holding");
+	}
+	
+	public void PlayThrowAnimation(){
+		Animator.Play("Armature_Throw");
+	}
+}
