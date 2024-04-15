264714399da3c9819ad6d3b5180e57b4d530fef2
Jude Holt <jude@holt-info.com>
2024-04-15 12:19:56 -0400

    Implemented more robust sound system

--- LudumDare55/Assets/Scripts/AudioController.cs
+++ LudumDare55/Assets/Scripts/AudioController.cs
@@ -1,23 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Class inspired by Brackeys tutorial: https://www.youtube.com/watch?v=6OT43pvUyfY
 public class AudioController : MonoBehaviour
 {
-    private AudioSource[] mysounds;
-
-    private AudioSource glassImpact;
-    private AudioSource metalImpact;
+    public Sound[] sounds;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
+    {
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play(string name)
     {
-        
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
     }
 }
