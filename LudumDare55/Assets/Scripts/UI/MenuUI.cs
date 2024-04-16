ab754a0c82cdb6aad2729905ea283fd78a1cf96a
Great <Okemakindetobi@gmail.com>
2024-04-16 02:04:12 +0100

    Level transition update

--- LudumDare55/Assets/Scripts/UI/MenuUI.cs
+++ LudumDare55/Assets/Scripts/UI/MenuUI.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MenuUI : MonoBehaviour
 {
+    public void OpenMainMenu()
+    {
+        if (LevelManager.Instance == null) return;
+
+        LevelManager.Instance.LoadMainMenu();
+    }
     public void StartGame()
     {
         if (LevelManager.Instance == null) return;
